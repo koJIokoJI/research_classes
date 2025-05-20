cfeb339ffc39906ff0124a38e0797a2583d0e178
mia <mxssedxnactxon@gmail.com>
2025-05-20 15:38:14 +0700

    пример использования класса

---
+++ wcexample1.py
@@ -0,0 +1,9 @@
+import wizcoin
+
+
+purse = wizcoin.WizCoin(2, 5, 99) # Целые числа передаются __init__()
+print(purse)
+print("G:", purse.galleons, "S:", purse.sickles, "K:", purse.knuts)
+print("Total value:", purse.value())
+print("Weight:", purse.weightInGrams(), "grams")
+print()
